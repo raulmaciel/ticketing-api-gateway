1697ed4475dab46e9612f9bc9af396b9ec960d54
raulmaciel <raulmacielxd@gmail.com>
2025-12-15 21:17:32 -0300

    feat: add routes for booking and inventory service, config apigateway port to 8090

---
+++ src/main/java/com/raulmaciel/apigateway/route/InventoryServiceRoutes.java
@@ -0,0 +1,26 @@
+package com.raulmaciel.apigateway.route;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class InventoryServiceRoutes {
+    @Bean
+    public RouterFunction<ServerResponse> inventoryRoutes() {
+        return route("inventory-service")
+                .GET("/api/v1/inventory/**", http())
+                .before(uri("http://localhost:8080"))
+                .build();
+    }
+}
+
